7143c70ea2db48f9c0d01c8f6dee03e58c6aa465
sidharthsaini18 <sidharth1812saini@gmail.com>
2025-06-07 21:09:29 +0530

    slidebar added

--- src/components/master.jsx
+++ src/components/master.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/header";
-import Sidebar from "./Sidebar";
+import Sidebar from "..components/Sidebar";
 
 const Master = () => {
   return (
