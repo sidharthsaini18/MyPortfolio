d89c01a59098952a4c495ab36929859ef9769514
sidharthsaini18 <sidharth1812saini@gmail.com>
2025-06-07 21:15:27 +0530

    / resolved

--- src/components/master.jsx
+++ src/components/master.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/header";
-import Sidebar from "..components/Sidebar";
+import Sidebar from "../components/Sidebar";
 
 const Master = () => {
   return (
